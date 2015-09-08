14d3608565199fbb487fb0235e53b5913e6c0d31
Debora Duarte <deboraaline@gmail.com>
2015-09-08 20:38:35 -0300

    Persisting data with an array

--- socke_io/app.js
+++ socke_io/app.js
@@ -3,17 +3,34 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
+var messages = [];
+
+var storeMessage = function(name, data) {
+  messages.push({ name: name, data: data });
+  if(messages.legth > 10) {
+    messages.shift();
+  }
+}
+
+var reloadMessages = function(client) {
+  messages.forEach(function(message){
+    client.emit('messages', message.name + ': ' + message.data);
+  });
+}
+
 io.on('connection', function(client){
 
   client.on('join', function(name) {
     console.log('Client ' + name + ' joined the chat...');
     client.name = name;
+    reloadMessages(client);
   });
 
   client.on('messages', function(data) {
     console.log(data);
-    client.broadcast.emit('messages', client.name + ': ' + data;
-    client.emit('messages', client.name + ': ' + data;
+    storeMessage(client.name, data);
+    client.broadcast.emit('messages', client.name + ': ' + data);
+    client.emit('messages', client.name + ': ' + data);
   });
 });
 
