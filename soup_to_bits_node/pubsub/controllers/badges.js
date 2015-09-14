11444f8f03a42be84b983722c2633d2bcd6e6c9a
Debora Duarte <deboraaline@gmail.com>
2015-09-14 17:37:21 -0300

    Implements publishing badges and getting the last 10 from Redis

--- soup_to_bits_node/pubsub/controllers/badges.js
+++ soup_to_bits_node/pubsub/controllers/badges.js
@@ -13,10 +13,30 @@ exports.save = function(request, response, next){
       return response.json(503, { data: data, error: true });
     }
     next();
+    model.trim();
   });
-}
+};
 
 //Send badges to pub/sub socket in model
 exports.send = function(request, response, next){
-  next();
-}
+  var badges = _.clone(request.body);
+
+  model.send(badges, function(error, data){
+    if (error) {
+      return response.json(503, { data: data, error: true });
+    }
+    response.json(200, { error: null });
+  });
+};
+
+/**
+  Get 10 badges from model
+*/
+exports.get = function(request, response){
+  model.get(function(error, data){
+    if (error) {
+      return response.json(503, { data: data, error: true });
+    }
+    response.json(200, data);
+  });
+};
--- soup_to_bits_node/pubsub/models/badge.js
+++ soup_to_bits_node/pubsub/models/badge.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var redis = require('../lib/redis');
+var broadcast = require('../lib/broadcast');
 
 /**
   Save badges to database
@@ -21,4 +22,32 @@ exports.save = function(badges, callback) {
     exports.save(badges, callback);
   });
 
+};
+
+/**
+  Trim down the redis list
+*/
+exports.trim = function(){
+  redis.ltrim('bagdes', 0, 9);
+};
+
+/**
+  Send out badges to the broadcaster
+  @param {Array} badges
+  @param {Function} callback
+*/
+exports.send = function(badges, callback){
+  badges.forEach(broadcast.send);
+  callback(null, null);
+};
+
+/**
+  Get badges from Redis
+  @param {Function} callback
+*/
+exports.get = function(callback) {
+  redis.lrange('badges', 0, -1, function(error, data){
+    if (error) return callback(error, null);
+    callback(null, data.map(JSON.parse));
+  });
 };
